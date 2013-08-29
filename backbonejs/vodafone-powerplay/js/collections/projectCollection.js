e1005854c48aa68835144061ca90e545a2216215
feeziman007 <feeziman007@hotmail.com>
2013-08-29 11:29:00 +1000

    removing unwanted js file

--- backbonejs/vodafone-powerplay/js/collections/projectCollection.js
+++
@@ -1,9 +0,0 @@
-/*
-	Collection
-*/
-
-Portfolio.projectCollections 		= Backbone.Collection.extend({
-	model: Portfolio.projectModel,
-	//url: 'http://localhost:8080/restler/apps/_syed-hafeez/projects'
-	url: 'http://api.syed-hafeez.com/restler/apps/_syed-hafeez/projects'
-});
